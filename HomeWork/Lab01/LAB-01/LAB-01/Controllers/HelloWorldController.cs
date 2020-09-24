eee38b268d1cb60694c0ea71bbf75680648c96db
OrangeFoie <vinhphuc931998@gmail.com>
2020-09-25 06:47:43 +0700

    update

---
+++ HomeWork/Lab01/LAB-01/LAB-01/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB_01.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        //public actionresult index()
+        //{
+        //    return view();
+        //}
+
+        // GET: HelloWorld
+        public String Index()
+        {
+            return "This is my <b> default <b> action...";
+        }
+
+        // Get:/HelloWorld/Wellcome
+        // Get:/HelloWorld/Wellcome?name=Scott&numtimes=4
+        //public String Wellcome(String name, int numtimes)
+        //{
+        //    //return "this is the wellcome to action method ...";
+        //    return HttpUtility.HtmlEncode("Hello " + name + " numtimes " + numtimes);   //link nhanh: https://localhost:44312/HelloWorld/Wellcome?name=Scott&numtimes=4
+        //}
+
+        // GET: /HelloWorld/Wellcome
+        public String Wellcome(String name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + id);
+        }
+    }
+}
