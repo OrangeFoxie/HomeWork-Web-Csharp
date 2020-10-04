9415f740a9e68b831e3eb9d85d545ecd08e27a52
OrangeFoie <vinhphuc931998@gmail.com>
2020-10-04 20:29:33 +0700

    LAB 01 web ban sach

---
+++ HomeWork/Web ban sach/Lab01-WebBanSach/MvcBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//Link to web https://localhost:44349/BookStore/Index
+
+namespace MvcBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        // GET: BookStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
